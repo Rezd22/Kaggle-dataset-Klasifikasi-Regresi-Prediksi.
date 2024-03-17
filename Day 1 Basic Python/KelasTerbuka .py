e98577d422283fb18365cb9a9a4c71d5f356450b
Rezd22 <prasanjayagalang22@gmail.com>
2024-03-17 14:15:05 +0700

    first commit

---
+++ Day 1 Basic Python/KelasTerbuka .py
@@ -0,0 +1,15 @@
+celcius = float(input("Masukan angka dalam celcius "))
+
+
+
+#reamur
+reamur = 4/5*celcius
+print(reamur)
+
+#fahrenheit
+fahremheit = (9/5* celcius) + 32
+print(fahremheit)
+
+#kelvin 
+kelvin = celcius + 273
+print(kelvin)
